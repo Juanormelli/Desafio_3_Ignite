53eb75c524a42a86cbab79e9721af50eb30a9a1f
Juan Lucas Ormelli de Medeiros <juan_ormelli@hotmail.com>
2021-08-25 09:16:59 -0300

    Desafio finalizado

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,27 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+      const user = await this.repository.find({relations:["games"], where:{id : user_id}})
+
+      return user[0];
+
+      
+
+
+      
+    
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("Select * from users order by first_name"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    const fn = first_name.toLowerCase();
+    const ln = last_name.toLowerCase()
+    return this.repository.query("Select * from users where lower(first_name)=lower($1) and lower(last_name)=lower($2)",[first_name,last_name] ); // Complete usando raw query
   }
 }
